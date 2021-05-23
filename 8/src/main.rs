08e40b2b96984f7f3a4cc1c5386c07d9d4cb6780
eda3 <gerorin1010@gmail.com>
2021-05-23 15:34:11 +0900

    fix: インデント修正

--- 8/src/main.rs
+++ 8/src/main.rs
@@ -2,22 +2,22 @@ use macroquad::prelude::*;
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
-    loop {
-        // 背景色を薄灰色に設定
-        clear_background(LIGHTGRAY);
+  loop {
+    // 背景色を薄灰色に設定
+    clear_background(LIGHTGRAY);
 
-　　　　// 青い棒を描画
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+    // 青い棒を描画
+    draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
 
-        // 緑の四角を描画
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+    // 緑の四角を描画
+    draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
 
-        // 黄色い丸を描画
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+    // 黄色い丸を描画
+    draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
 
-        // 濃灰色で文字を描画
-        draw_text("HELLO", 20.0, 20.0, 30.0, DARKGRAY);
+    // 濃灰色で文字を描画
+    draw_text("HELLO", 20.0, 20.0, 30.0, DARKGRAY);
 
-        next_frame().await
-    }
+    next_frame().await
+  }
 }
